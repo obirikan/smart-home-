338bf9dfe5f75f237f60575575b868ea867d1d69
obirikan <obirikan020@gmail.com>
2025-08-06 22:01:23 +0000

    auth+persistent login

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,17 +1,9 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore'; 
+import { firebaseConfig } from '@/env/main';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDqr-RUgHbk85X57F3n8MrsT4iK_mcaSa8",
-  authDomain: "letter-duel1.firebaseapp.com",
-  databaseURL: "https://letter-duel1-default-rtdb.firebaseio.com",
-  projectId: "letter-duel1",
-  storageBucket: "letter-duel1.firebasestorage.app",
-  messagingSenderId: "268073447618",
-  appId: "1:268073447618:web:abe39fecc9d80556930b5b",
-  measurementId: "G-GED3TLQNL7"
-};
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
